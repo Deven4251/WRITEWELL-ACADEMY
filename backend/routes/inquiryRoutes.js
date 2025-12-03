8045e846512dd1af97dc3511faa0e9e2d00c73a5
DEVENDRA MISHRA <devendramishra778585@gmail.com>
2025-12-03 22:10:34 +0530

    commit 3

--- backend/routes/inquiryRoutes.js
+++ backend/routes/inquiryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { handleInquiry } = require("../controllers/inquiryController");
+const { handleInquiry } = require("../controllers/InquiryController");
 
 router.post("/", handleInquiry);
 
